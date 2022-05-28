2aa662a8e7c2018c8c8ec4455db4a5899d094560
Vi <silvia.copil@gmail.com>
2022-05-28 17:42:23 +0200

    .

--- Week 9 homework/Week9Homework.c
+++ Week 9 homework/Week9Homework.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+// Recursivity - Stack killer
+unsigned int recursive_power(unsigned int a, unsigned int n) {
+	if (n == 0) {
+		return 1;
+	}
+	else {
+		return a * recursive_power(a, n - 1);
+	}
+}
+
+// O(n) liniar implementation
 unsigned int liniar_power(unsigned int a, unsigned int n) {
 	unsigned int power = 1;
 
@@ -10,32 +21,15 @@ unsigned int liniar_power(unsigned int a, unsigned int n) {
 	return power;
 }
 
-unsigned int recursive_power(unsigned int a, unsigned int n) {
-	unsigned int power = 1;
-
-	if (n == 0) {
-		return 1;
-	}
-	power = recursive_power(a, n / 2);
-	if (n % 2 == 0) {
-		return power * power;
-	}
-	else {
-		return a * power * power;
-	}
-}
-
+// O(logn) implementation with bit shift operations
 unsigned int bit_power(unsigned int a, unsigned int n) {
 	unsigned int power = 1;
 
 	while (n > 0) {
 		if (n & 1u) {
-			printf("%d \n", n);
 			power = a * power;
 		}
-
 		n = n >> 1;
-		printf("%d \n", n);
 		a = a * a;
 	}
 
@@ -45,7 +39,6 @@ unsigned int bit_power(unsigned int a, unsigned int n) {
 
 int main() {
 
-	printf("Hello world!\n");
 	printf("recursive:  %d \n", recursive_power(2, 11));
 	printf("liniar:  %d \n", liniar_power(2, 11));
 	printf("bit shift:  %d \n", bit_power(2, 11));
